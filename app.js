0ed3fb0dc00eef71f21308cbc046fc8482875898
Paul Savignano <Paul.Savignano@gmail.com>
2016-12-30 07:38:53 -0800

    Add store methods.

--- app.js
+++ app.js
@@ -8,16 +8,34 @@ function reducer(state, action) {
   }
 }
 
-const incrementAction = { type: 'INCREMENT', amount: 7 };
-console.log(reducer(0, incrementAction));
-console.log(reducer(1, incrementAction));
-console.log(reducer(5, incrementAction));
+function createStore() {
+  let state = 0;
+  const getState = () => (state);
+  const dispatch = (action) => {
+    state = reducer(state, action)
+  };
+  return {
+    getState,
+    dispatch
+  }
+}
 
-const unknownAction = { type: 'UNKNOWN' };
-console.log(reducer(5, unknownAction));
-console.log(reducer(8, unknownAction));
+const store = createStore(reducer);
 
-const decrementAction = { type: 'DECREMENT' }
-console.log(reducer(10, decrementAction));
-console.log(reducer(9, decrementAction));
-console.log(reducer(5, decrementAction));
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 3
+};
+
+store.dispatch(incrementAction);
+console.log(store.getState());
+store.dispatch(incrementAction);
+console.log(store.getState());
+
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 4,
+};
+
+store.dispatch(decrementAction);
+console.log(store.getState());
